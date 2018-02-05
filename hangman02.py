db75f2c79a4b7df4e0a4c8e2ee5f4fc0f30baee8
qgmcxthvie <jasonculligan@gmail.com>
2018-02-05 22:17:55 +0000

    Added break after easter egg is found

--- hangman02.py
+++ hangman02.py
@@ -67,6 +67,7 @@ while win == 1:
                     break
                 if goagain == "cabbage":
                     print "You've found the easter egg!!"
+                    break
                 else:
                     if gameswon == 1:
                         print "\nYou won 1 game. You got", runningtotal, "out of 10 which is", 100 * runningtotal / (totgames * 10),"%",2*"\n"
